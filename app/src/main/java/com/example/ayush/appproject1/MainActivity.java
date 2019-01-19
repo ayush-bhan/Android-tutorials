7880aac205e4f727cf300c7fef57a6344084c443
Ayush Bhan <32763933+ayush-bhan@users.noreply.github.com>
2019-01-19 21:48:47 +0530

    init

--- app/src/main/java/com/example/ayush/appproject1/MainActivity.java
+++ app/src/main/java/com/example/ayush/appproject1/MainActivity.java
@@ -5,24 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText eta;
-    Button btn;
-
+    Button btna, btnb;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         eta= (EditText) findViewById(R.id.et);
-        btn=findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btna=findViewById(R.id.btn1);
+        btnb=findViewById(R.id.btn2);
+        tv=findViewById(R.id.tv1);
+        btna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s = eta.getText().toString();
                 Toast.makeText(getBaseContext(),s,Toast.LENGTH_SHORT).show();
             }
         });
+
+      btnb.setOnClickListener(new View.OnClickListener() {
+            @Override
+          public void onClick(View v) {
+                tv.setText(eta.getText().toString());
+            }
+       });
     }
 }
